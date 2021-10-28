17ce3453bf1279785fad25ccd2bbfbb2f78cd5d4
Jacek <87030915+jaceksl1@users.noreply.github.com>
2021-10-28 20:17:22 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,25 +4,25 @@ function Footer() {
   return (
     <footer>
       <p>Created by Jacek Słomian</p>
-      <div class="contact-links">
+      <div className="contact-links">
         <a
           href="https://www.linkedin.com/in/jacek-s%C5%82omian-759171b9/"
           target="_blank"
-          class="btn contact-details">
+          className="btn contact-details">
           <i class="fab fa-facebook-square"></i> LinkedIn
         </a>
         <a
           id="profile-link"
           href="https://github.com/jaceksl1"
           target="_blank"
-          class="btn contact-details">
-          <i class="fab fa-github"></i> GitHub
+          className="btn contact-details">
+          <i className="fab fa-github"></i> GitHub
         </a>
-        <a href="mailto:j.k.slomian@gmail.com" class="btn contact-details">
-          <i class="fas fa-at"></i> Send a mail
+        <a href="mailto:j.k.slomian@gmail.com" className="btn contact-details">
+          <i className="fas fa-at"></i> Send a mail
         </a>
-        <a href="tel:691-247-642" class="btn contact-details">
-          <i class="fas fa-mobile-alt"></i> Call me
+        <a href="tel:691-247-642" className="btn contact-details">
+          <i className="fas fa-mobile-alt"></i> Call me
         </a>
       </div>
     </footer>
